0c69fafb4a9888a4bec20454e16a255e730c8729
Jamon Camisso <jamonation@gmail.com>
2014-10-21 20:01:13 -0400

    finalizing docker instructions, build script, and environment

--- dockerfiles/build_docker_images.sh
+++ dockerfiles/build_docker_images.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+BOOT2DOCKER=$(which boot2docker)
+if [ $? -eq 0 ]
+then
+    BOOT2DOCKERIP=$(boot2docker ip 2>&1|awk '{print $NF}'|grep -v "^$")
+    SITE="$BOOT2DOCKERIP"
+    printf "\n\033[1mFor reference, it looks like you're running boot2docker\033[m\n"
+    printf "\n\033[1mPlease make a note of this if this setup fails and you need some help\033[m\n"
+else
+    SITE="127.0.0.1"
+fi
+
 printf "\033[1mInstalling fig (http://www.fig.sh/) to manage docker development environment\033[m\n"
 pip install fig
 
 printf "\n\033[1mCreating data container for MySQL persistence:\033[m\n"
-docker build -t ateam/alert_manager_data dockerfiles/alert_manager_data
+docker build -t ateam/alert_manager_data alert_manager_data
 docker run --name data ateam/alert_manager_data
 if [ $? -eq 1 ]
 then
@@ -15,16 +26,21 @@ then
 fi
 
 printf "\033[1mCreating MySQL application container\033[m\n"
-docker build -t ateam/alert_manager_db dockerfiles/alert_manager_db
+docker build -t ateam/alert_manager_db alert_manager_db
 docker run --volumes-from data ateam/alert_manager_db /usr/local/bin/provision_mysql.sh
 
-BOOT2DOCKER=$(which boot2docker)
-if [ $? -eq 0 ]
-then
-    BOOT2DOCKERIP=$(boot2docker ip 2>&1|awk '{print $NF}'|grep -v "^$")
-    SITE="$BOOT2DOCKERIP"
-    printf "\033[1mFor reference, it looks like you're running boot2docker\033[m\n"
-else
-    SITE="127.0.0.1"
-fi
-    printf "\033[1mAll done! Run 'fig up' and then browse to http://$SITE:8080/alerts.html\033[m\n"
+printf "\033[1mCreating alert_manager development application container\033[m\n"
+docker build -t ateam/alert_manager_web alert_manager_web
+
+
+cp fig.yml.template fig.yml
+SHARED_DIR=$(echo $PWD|sed 's/dockerfiles/alert_manager/')
+printf "  volumes:\n" >> fig.yml
+printf "    - $SHARED_DIR:/srv/www/alert_manager\n" >> fig.yml 
+
+printf "\033[1mCheck '\033[31mfig.yml\033[m\033[1m' in this directory.\033[m\n"
+printf "\033[1mEnsure that the volumes: directive to points to your shared folder\033[m\n"
+printf "\033[1mIt should be something like $SHARED_DIR:/srv/www/alert_manager\033[m\n"
+printf "\033[1mAfter you check/edit, you should be all set!\033[m\n"
+printf "\n\033[1mWhen you're ready, run '\033[31mfig up\033[m\033[1m' and browse to:\nhttp://$SITE:8080/alerts.html\033[m\n"
+
